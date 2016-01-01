2cf3362cf1d3fe3ef39592ef6d5a3a560f2511a0
Cole Chamberlain <cole.chamberlain@gmail.com>
2015-12-31 21:12:42 -0800

    minor require fixes

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ var Color = require('./lib/Color');
 var Display = require('./lib/Display');
 
 var assign = require('object-assign');
-var curry = require('./curry');
+var curry = require('./lib/curry');
 
 var index = assign({curry: curry}, Color, Display, Addons);
 
