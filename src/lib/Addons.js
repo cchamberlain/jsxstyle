8530cdd59821ef95ce6f65e3a20b628f2cf62cfe
Cole Chamberlain <cole.chamberlain@gmail.com>
2015-12-31 21:16:05 -0800

    missed one require fix

--- src/lib/Addons.js
+++ src/lib/Addons.js
@@ -1,8 +1,8 @@
 'use strict';
 
-var Flex = require('../Flex');
+var Flex = require('./Flex');
 
-var curry = require('../curry');
+var curry = require('./curry');
 
 var Addons = {
   Row: curry(Flex, {flexDirection: 'row'}),
